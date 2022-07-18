fba7264dfd0376f8b5064b676934394668daa1c3
shahabahmedlps <96421681+shahabahmedlps@users.noreply.github.com>
2022-07-18 13:58:30 +0500

    Delete .tmp directory

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var barChartFB8AD568998F44839CEA0521EABDFBE6: IVisualPlugin;
-export default barChartFB8AD568998F44839CEA0521EABDFBE6;
